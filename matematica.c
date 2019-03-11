48b373bba29724573f1105f55720fb19ee548960
Pedro Cantele <pedro.cantele2@hotmail.com>
2019-03-11 15:11:29 -0300

    Adiciona prototipo

--- matematica.c
+++ matematica.c
@@ -2,7 +2,14 @@
 int soma(int a, int b) {
 	int res = a + b;
 	return res;
+}
 int sub(int a, int b) {
 	int res = a - b;
 	return res;
+}
+int mult(int a, int b){
+
+}
+int quad(int a){
+
 }
--- matematica.h
+++ matematica.h
@@ -1,3 +1,5 @@
 // Biblioteca matemática
 int soma(int a, int b);
 int sub(int a, int b);
+int mult(int a, int b);
+int quad(int a);
